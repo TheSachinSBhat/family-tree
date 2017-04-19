59f3052808c47406fe13a150f344bf89a38b1fd4
Sachin Bhat <bhat.sachinsbhat@gmail.com>
2017-04-20 00:26:29 +0530

    Commit #1: Port code from Digital Diary

---
+++ gettingstarted/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+import hello.views
+
+# Examples:
+# url(r'^$', 'gettingstarted.views.home', name='home'),
+# url(r'^blog/', include('blog.urls')),
+
+urlpatterns = [
+    url(r'^$', hello.views.index, name='index'),
+    url(r'^db', hello.views.db, name='db'),
+    url(r'^dashboard', hello.views.dashboard, name='dashboard'),
+    url(r'^ideas', hello.views.ideas, name='ideas'),
+    url(r'^admin/', include(admin.site.urls)),
+]
